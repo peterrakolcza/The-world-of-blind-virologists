a975b839a53216a14769e48fce9d11beb708d494
Stefler Gábor <gabor.stefler@gmail.com>
2022-04-25 13:10:19 +0200

    futtato file es projekt refactor

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
-javac.exe -d bin -sourcepath src src/Sequences.java -cp "./lib/gson-2.9.0.jar"
+javac.exe -d bin -sourcepath src src/tests/Sequences.java -cp "./lib/gson-2.9.0.jar"
 
-java.exe -cp "./bin;./lib/gson-2.9.0.jar" Sequences
+java.exe -cp "./bin;./lib/gson-2.9.0.jar" tests.Sequences
 
 
 
--- src/tests/Sequences.java
+++ src/tests/Sequences.java
@@ -1,3 +1,4 @@
+package tests;
 import businesslogic.*;
 
 import java.io.IOException;
